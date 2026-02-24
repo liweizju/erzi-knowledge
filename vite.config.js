d05a3bf8ea8ecfe0c994a1b07fdb444ab0c407c2
julewizlab <leewei.david@gmail.com>
2026-02-24 23:47:52 +0800

    fix: avoid underscored chunk filenames for gh-pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/entry-[hash].js',
+        chunkFileNames: 'assets/chunk-[hash].js',
+      },
+    },
+  },
   server: {
     port: 3001,
   },
